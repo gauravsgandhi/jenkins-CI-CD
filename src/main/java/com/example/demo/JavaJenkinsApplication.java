fcc5eaaa5f08e7c2dd345f3bc9c4e2fd07bddc52
gaurav <gauravsgandhi@gmail.com>
2022-06-23 14:16:24 +0530

    first commit

---
+++ src/main/java/com/example/demo/JavaJenkinsApplication.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JavaJenkinsApplication {
+
+	@GetMapping("/welcome")
+	public String Welcome(){
+		return "Welcome to jenkins CI CD Pipeline";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/JavaJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
